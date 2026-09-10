ad32e783829fe1755d2bed02c586526c031ff445
joetheproz5 <joetheproz5@users.noreply.github.com>
2026-09-10 19:08:39 +0300

    Remove stretched cable packet on responsive route

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "hart-el-sett-power-v15";
-const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.8", "./light.css?v=20260910.5", "./app.js?v=20260910.10", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "hart-el-sett-power-v16";
+const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.9", "./light.css?v=20260910.6", "./app.js?v=20260910.10", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
