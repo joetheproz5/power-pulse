1b7627b9bbf8f49e82f308da12845269af8359e2
joetheproz5 <joetheproz5@users.noreply.github.com>
2026-09-10 23:35:08 +0300

    Polish live-status launch animation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "hart-el-sett-power-v44";
-const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.34", "./app.js?v=20260910.26", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "hart-el-sett-power-v45";
+const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.35", "./app.js?v=20260910.27", "./manifest.webmanifest", "./icon.svg"];
 const NOTIFICATION_CACHE = "hart-el-sett-notification-state-v1";
 const NOTIFICATION_PREFERENCE = new Request(new URL("./notification-preference", self.location.origin));
 const NOTIFICATION_STATE = new Request(new URL("./notification-state", self.location.origin));
