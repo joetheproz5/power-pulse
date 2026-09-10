f4eccb191647a84442eb461ab4b9d40c55664354
joetheproz5 <joetheproz5@users.noreply.github.com>
2026-09-10 19:24:13 +0300

    Unify source devices into home scene

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "hart-el-sett-power-v20";
-const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.12", "./light.css?v=20260910.8", "./app.js?v=20260910.11", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "hart-el-sett-power-v21";
+const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.13", "./light.css?v=20260910.9", "./app.js?v=20260910.11", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
