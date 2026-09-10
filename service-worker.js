bd5a0e2c05fcb108f233889d9fe571bd2d824088
joetheproz5 <joetheproz5@users.noreply.github.com>
2026-09-10 18:54:21 +0300

    Adopt professional light equipment theme

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "hart-el-sett-power-v8";
-const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.2", "./app.js?v=20260910.8", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "hart-el-sett-power-v9";
+const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.2", "./light.css?v=20260910.1", "./app.js?v=20260910.8", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
