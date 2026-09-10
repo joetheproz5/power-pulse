d4ea550fceb186b946bfcef32ed6be202ec1fb4f
joetheproz5 <joetheproz5@users.noreply.github.com>
2026-09-10 18:59:41 +0300

    Make live cable current animation prominent

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "hart-el-sett-power-v10";
-const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.3", "./light.css?v=20260910.2", "./app.js?v=20260910.8", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "hart-el-sett-power-v11";
+const APP_FILES = ["./", "./index.html", "./styles.css?v=20260910.8", "./refinements.css?v=20260910.4", "./light.css?v=20260910.3", "./app.js?v=20260910.8", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES))));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
